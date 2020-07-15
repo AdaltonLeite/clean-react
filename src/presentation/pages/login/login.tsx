b601b084e1a97ca89579bdc985249a3d3ef1734d
Adalton <juniotsl@gmail.com>
2020-07-15 19:40:01 -0300

    chore: add files to coverage ignore

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,7 +4,7 @@ import Styles from './login-styles.scss'
 import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
-import { Authentication } from '@/domain/usecases'
+import { Authentication } from '@/domain/usecases/authentication'
 
 type Props = {
   validation: Validation
